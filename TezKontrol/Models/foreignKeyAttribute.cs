00a35303f13d297e0d9f812d17464c6cb7a44d41
ceymi <ceymi@DESKTOP-NU1L5FK>
2022-10-08 20:26:25 +0300

    Güncellemeler Ve Butonlar Eklendi

---
+++ TezKontrol/Models/foreignKeyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TezKontrol.Models
+{
+    internal class foreignKeyAttribute : Attribute
+    {
+        private string v;
+
+        public foreignKeyAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
